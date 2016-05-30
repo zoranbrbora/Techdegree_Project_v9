726b1a9ac9866293e3d3d4e269fe98010c6ab5c0
Zoran Brbora <zoranbrbora@yahoo.co.uk>
2016-05-30 20:55:47 +0200

    Finished alert message.

--- js/webapp.js
+++ js/webapp.js
@@ -1,24 +1,13 @@
 var  $notBtn = $('#notificationBtn'),
-	 $alerts = $('#alerts'),
 	 $alertLight = $('#alert-light'),
 	 $message1 = $('#message1'),
-	 $message2 = $('#message2'),
-	 $offBtn1 = $('#offButton1'),
-	 $offBtn2 = $('#offButton2');
-
+	 $offBtn1 = $('#offButton1');
+     
 $offBtn1.click("on" ,function(){
     $message1.hide(500);
-    	$alertLight.hide();	
 });
 
-$offBtn2.click("on" ,function(){
-    $message2.hide(500);
-    $alertLight.hide();
-});
 
-$notBtn.click("on" ,function(){
-    $alerts.toggle(500);
-});
 
 
 
