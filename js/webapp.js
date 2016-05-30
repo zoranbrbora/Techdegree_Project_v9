eed0632cfbf0d998085c447f76d64ba14a068113
Zoran Brbora <zoranbrbora@yahoo.co.uk>
2016-05-30 21:00:22 +0200

    Added animation to alert message.

--- js/webapp.js
+++ js/webapp.js
@@ -2,9 +2,9 @@ var  $notBtn = $('#notificationBtn'),
 	 $alertLight = $('#alert-light'),
 	 $message1 = $('#message1'),
 	 $offBtn1 = $('#offButton1');
-     
+
 $offBtn1.click("on" ,function(){
-    $message1.hide(500);
+    $message1.slideUp(500);
 });
 
 
