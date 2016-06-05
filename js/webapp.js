4188d5fca69e76be17eac49059e1accec0dd7e42
Zoran Brbora <zoranbrbora@yahoo.co.uk>
2016-06-05 14:15:03 +0200

    Finished Member Message Box.

--- js/webapp.js
+++ js/webapp.js
@@ -1,13 +1,30 @@
-var  $notBtn = $('#notificationBtn'),
-	 $alertLight = $('#alert-light'),
-	 $message1 = $('#message1'),
+var  $message1 = $('#message1'),
 	 $offBtn1 = $('#offButton1');
 
+
+// ALERT CLOSE
 $offBtn1.click("on" ,function(){
     $message1.slideUp(500);
 });
 
 
+// SENDING MESSAGE
+$("form").click(function(){
+    $(".nav li").removeAttr('id');
+   	$(this).attr('id');
+});
 
+$("#send").click(function(){
+ 	if (!$(".user-message").val()) {
+   		$(".warning-message").text("Please enter a message").show();
+	} else {
+		$(".warning-message").text("Your Message is on the way!");
+	}
+});
+
+// MEMBERS AUTOCOMPLETE SEARCH
+$( "#autocomplete" ).autocomplete({
+  source: [ "Victoria Chambers", "Dale Byrd", "Dawn Wood", "Dan Oliver" ]
+});
 
 
